55a48b3e048a085c441d20cde072770bf574af2c
WideHardo7 <marzilianoluca@yahoo.com>
2024-09-25 19:32:50 +0200

    hmmm

--- MonitorController.lua
+++ MonitorController.lua
@@ -20,7 +20,7 @@ end
 
 disegnaBottone(colore)
 
--- Funzione che gestisce il ciclo redstone
+-- Funzione per gestire il ciclo Redstone
 function cicloRedstone()
     while running do
         redstone.setOutput(redstoneSide, true)
@@ -30,21 +30,22 @@ function cicloRedstone()
     end
 end
 
--- Funzione che gestisce il monitor touch
-function gestisciTouch()
-    while true do
-        local evento, lato, x, y = os.pullEvent("monitor_touch")
-        if x >= 1 and x <= 15 and y == 1 then
-            colore = not colore
-            disegnaBottone(colore)
-            running = not running -- Invertiamo lo stato di running
-            if not running then
-                redstone.setOutput(redstoneSide, false) -- Assicuriamoci di spegnere il redstone quando fermiamo
-            end
+while true do
+    local evento, lato, x, y = os.pullEvent("monitor_touch")
+    if x >= 1 and x <= 15 and y == 1 then
+        -- Cambiamo stato del bottone
+        colore = not colore
+        disegnaBottone(colore)
+
+        -- Invertiamo lo stato del ciclo Redstone
+        running = not running
+
+        if running then
+            -- Se running è true, iniziamo il ciclo Redstone in un thread separato
+            parallel.waitForAny(cicloRedstone)
+        else
+            -- Se running è false, assicuriamoci che il redstone sia spento
+            redstone.setOutput(redstoneSide, false)
         end
     end
 end
-
--- Avviamo entrambe le funzioni in parallelo
-parallel.waitForAny(cicloRedstone, gestisciTouch)
-
