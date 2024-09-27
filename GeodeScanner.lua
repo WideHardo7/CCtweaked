e1087edd161a6e75e59e99dd3955293f95a54454
WideHardo7 <marzilianoluca@yahoo.com>
2024-09-27 13:00:54 +0200

    chadgpt

--- GeodeScanner.lua
+++ GeodeScanner.lua
@@ -1,7 +1,16 @@
 -- QUESTO FILE USA L'UNIVERSAL SCANNER DELLA UNLIMITED PERIPHERAL WORKS
 
-local args= {...}
-local scanner = peripheral.wrap("down")
+local args = {...}
+local scannerSide = "down"
+
+if not peripheral.isPresent(scannerSide) then
+    error("No peripheral found on side: " .. scannerSide)
+end
+
+local scanner = peripheral.wrap(scannerSide)
+if peripheral.getType(scannerSide) ~= "universal_scanner" then
+    error("Peripheral on side " .. scannerSide .. " is not a Universal Scanner.")
+end
 
 function redstonePulse()
     redstone.setOutput("back", true)
@@ -9,15 +18,15 @@ function redstonePulse()
     redstone.setOutput("back", false)
 end
 
-
 while true do
-    local risultati = scanner.scan("block",1)
-    for block in risultati do
-        if block["name"]==args[1] then
-            print("Blocco Trovato: ".. block["name"].. " in  posizione x: ".. block["x"] .. " y: "..block["y"].. " z: "..block["z"] )
+    local results = scanner.scan("block", 1)
+    for _, block in pairs(results) do
+        if block.name == args[1] then
+            print("Blocco Trovato: " .. block.name .. " in posizione x: " .. block.x .. " y: " .. block.y .. " z: " .. block.z)
             redstonePulse()
         else
-             print("Blocco diverso dal cercato trovato: ".. block["name"] .. " in  posizione x: ".. block["x"] .. " y: "..block["y"].. " z: "..block["z"] )
+            print("Blocco diverso dal cercato trovato: " .. block.name .. " in posizione x: " .. block.x .. " y: " .. block.y .. " z: " .. block.z)
         end
     end
+    sleep(1) -- Add a small delay to prevent excessive looping
 end
