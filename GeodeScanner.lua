545aebd5faa9633d5c4ab9c372c0d2fecb69b301
WideHardo7 <marzilianoluca@yahoo.com>
2024-09-27 14:21:09 +0200

    fix

--- GeodeScanner.lua
+++ GeodeScanner.lua
@@ -2,28 +2,31 @@
 
 local args = {...}
 
--- Trova lo scanner sulla rete
+-- Trova lo scanner
 local scanner = peripheral.find("universal_scanner")
-
 if not scanner then
-    error("Scanner universale non trovato sulla rete.")
+    error("Universal Scanner non trovato")
 end
 
 function redstonePulse()
-    redstone.setOutput("right", true)
+    redstone.setOutput("back", true)
     sleep(1)
-    redstone.setOutput("right", false)
+    redstone.setOutput("back", false)
 end
 
 while true do
     local risultati = scanner.scan("block", 1)
-    for _, block in pairs(risultati) do
-        if block.name == args[1] then
-            print("Blocco Trovato: " .. block.name .. " in posizione x: " .. block.x .. " y: " .. block.y .. " z: " .. block.z)
-            redstonePulse()
-        else
-            print("Blocco diverso dal cercato trovato: " .. block.name .. " in posizione x: " .. block.x .. " y: " .. block.y .. " z: " .. block.z)
+    if risultati then
+        for _, block in pairs(risultati) do
+            if block.name == args[1] then
+                print("Blocco Trovato: " .. block.name .. " in posizione x: " .. block.x .. " y: " .. block.y .. " z: " .. block.z)
+                redstonePulse()
+            else
+                print("Blocco diverso dal cercato trovato: " .. block.name .. " in posizione x: " .. block.x .. " y: " .. block.y .. " z: " .. block.z)
+            end
         end
+    else
+        print("Nessun blocco rilevato nella scansione.")
     end
-    sleep(1) -- Aggiungi un piccolo ritardo per evitare cicli eccessivi
+    sleep(1)  -- Aggiungi una pausa per evitare loop troppo rapidi
 end
